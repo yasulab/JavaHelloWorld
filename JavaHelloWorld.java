6cfba971e7effaec6ab550d65f60b82b6ab30ef1
Yohei YASUKAWA <yasulab@gmail.com>
2016-04-02 21:10:25 +0900

    Fix typo

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -1,6 +1,6 @@
-public classs JavaHelloWorld{
-  public static void main (String [] args){
-    System.out.println("Java Hello World");
+public class JavaHelloWorld{
+    public static void main (String [] args){
+	System.out.println("Java Hello World");
 	System.out.println("Hello again");
-  }
+    }
 }
