bddeec82e623c6702ab53f39db6550ef0c39f10f
Yohei YASUKAWA <yasulab@gmail.com>
2016-04-02 21:15:38 +0900

    Add one more hello

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,5 +2,6 @@ public class JavaHelloWorld{
     public static void main (String [] args){
 	System.out.println("Java Hello World");
 	System.out.println("Hello again");
+	System.out.println("hello three times");
     }
 }
